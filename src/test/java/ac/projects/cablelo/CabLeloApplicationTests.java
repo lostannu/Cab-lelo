773729d0c9089fe40c9fda3741ef8fad5c3ef4c2
lostannu <icyanupam@gmail.com>
2025-05-13 20:13:18 +0530

    Initial Commit

---
+++ src/test/java/ac/projects/cablelo/CabLeloApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.projects.cablelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabLeloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
