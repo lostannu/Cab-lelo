a88b0b9fc1788540b293231bf324c47fdaa6e6c4
lostannu <icyanupam@gmail.com>
2025-05-16 14:07:24 +0530

    controllers

--- src/main/java/ac/projects/cablelo/controller/UserController.java
+++ src/main/java/ac/projects/cablelo/controller/UserController.java
@@ -3,6 +3,8 @@ import ac.projects.cablelo.model.User;
 import ac.projects.cablelo.service.UserService;
 import ch.qos.logback.core.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,8 +36,8 @@ public class UserController {
         return userService.createUser(user);
     }
     @PutMapping("/{id}")
-    public User updateUser(@PathVariable String id ,@RequestBody User user){
-        return userService.updateUser(id,user);
+    public ResponseEntity<User> updateUser(@PathVariable String id ,@RequestBody User user){
+        return userService.updateUser(id, user);
     }
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable String id){
--- src/main/java/ac/projects/cablelo/service/UserService.java
+++ src/main/java/ac/projects/cablelo/service/UserService.java
@@ -4,6 +4,9 @@ package ac.projects.cablelo.service;
 import ac.projects.cablelo.model.User;
 import ac.projects.cablelo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,13 +35,13 @@ public class UserService {
         return userRepository.findById(id).orElse(null);
     }
 
-    public User updateUser(String id, User updatedUser) {
+    public ResponseEntity<User> updateUser(String id, User updatedUser) {
         Optional<User> existing = userRepository.findById(id);
         if (existing.isPresent()) {
             updatedUser.setId(id);
-            return userRepository.save(updatedUser);
+            return new ResponseEntity(userRepository.save(updatedUser), HttpStatus.OK);
         }
-        return null;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     public String deleteUser(String id) {
